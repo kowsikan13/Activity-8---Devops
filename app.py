a0ed47bf385b23eef7d3aa436680f5d17ca7e04d
kowsikanA <kowsikan.a@gmail.com>
2025-11-10 22:01:06 -0500

    Part 8: add POST /echo and /message GET/PUT/DELETE with tests

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +6,13 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!")
+
+
+# In app.py
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
